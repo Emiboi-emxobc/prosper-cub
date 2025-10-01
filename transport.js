5595c92052143ae1d55609626fbf0f8b111463d6
Emiboi-emxobc <echukwuemeka947@gmail.com>
2025-10-01 14:05:02 +0100

    Made some changes in transport.js

---
+++ transport.js
@@ -0,0 +1,43 @@
+const API_URL = "https://prosper-cub-1.onrender.com";
+
+import {validateForm} from './helper.js';
+
+export async function submitform(form,type = "submit-login"){
+  
+ output.textContent = "Please wait...";
+
+
+      try {
+        const response = await fetch(`${API_URL}/${type}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          output.textContent = "Authentication required!";
+          window.location = "auth.html";
+        } else {
+          output.textContent = "❌ Error: " + data.error;
+        }
+
+        console.log("Response:", data);
+      } catch (err) {
+  output.textContent = "❌ Network error, check console.";
+  console.error("Fetch failed:", err);
+  console.log("Network error: " + err.message);
+  
+  
+  if (form.type === "submit-vote") {
+    submitSecform(form);
+  }
+}
+      
+      
+
+
+    
+
+}
